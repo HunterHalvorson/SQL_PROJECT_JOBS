9d1a71f07e1c9a42bc25920108f8b4567b525ce4
HunterHalvorson <126722998+HunterHalvorson@users.noreply.github.com>
2025-08-13 18:43:27 -0600

    project

---
+++ project_sql/5_most_optimal_skill.sql
@@ -0,0 +1,52 @@
+/*
+Question: What are the most optimal skills to learn(in demand and high paying)
+*/
+
+/*
+Question: What are the most optimal skills to learn (in demand and high paying)
+*/
+
+WITH skills_demand AS (
+  SELECT 
+    skills_job_dim.skill_id,
+    skills,
+    COUNT(skills_job_dim.job_id) AS demand_count
+  FROM job_postings_fact
+  INNER JOIN skills_job_dim 
+    ON job_postings_fact.job_id = skills_job_dim.job_id
+  INNER JOIN skills_dim 
+    ON skills_job_dim.skill_id = skills_dim.skill_id
+  WHERE
+    job_title_short = 'Data Analyst' 
+    AND job_work_from_home = TRUE
+    AND salary_year_avg IS NOT NULL
+  GROUP BY skills_job_dim.skill_id, skills
+),
+average_salary AS (
+  SELECT 
+    skills_job_dim.skill_id,
+    skills,
+    ROUND(AVG(salary_year_avg), 2) AS avg_salary
+  FROM job_postings_fact
+  INNER JOIN skills_job_dim 
+    ON job_postings_fact.job_id = skills_job_dim.job_id
+  INNER JOIN skills_dim 
+    ON skills_job_dim.skill_id = skills_dim.skill_id
+  WHERE
+    job_title_short = 'Data Analyst' 
+    AND job_work_from_home = TRUE
+    AND salary_year_avg IS NOT NULL
+  GROUP BY skills_job_dim.skill_id, skills
+)
+SELECT 
+  sd.skill_id,
+  sd.skills,
+  sd.demand_count,
+  asal.avg_salary
+FROM skills_demand sd
+JOIN average_salary asal
+  ON sd.skill_id = asal.skill_id
+ORDER BY 
+  sd.demand_count DESC,
+  asal.avg_salary DESC
+LIMIT 20;
